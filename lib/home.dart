04eb22b222bf95908702a83d245542fcddf9b566
Jacob Sock <jacobmsock@gmail.com>
2024-10-11 09:30:08 -0400

    implement example _buildGridCards function with dummy data

--- lib/home.dart
+++ lib/home.dart
@@ -17,7 +17,38 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(
+      count,
+      (int index) {
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18.0 / 11.0,
+                child: Image.asset('assets/diamond.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Text('Title'),
+                    SizedBox(height: 8.0),
+                    Text('Secondary Text'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+    return cards;
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -61,41 +92,11 @@ class HomePage extends StatelessWidget {
       // TODO: Add a grid view (102)
 
       body: GridView.count(
-        crossAxisCount: 3,
-        padding: const EdgeInsets.all(16.0),
-        //The parent GridView, defines aspect ratio for children (Cards).
-        childAspectRatio: 8.0 / 9.0,
-        children: <Widget>[
-          Card(
-            surfaceTintColor: const Color.fromARGB(255, 47, 133, 176),
-            shadowColor: Colors.orange,
-            elevation: 50,
-            clipBehavior: Clip.antiAlias,
-            
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                //defines the aspect ratio the image regardless of shape
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Title'),
-                      const SizedBox(height: 8.0),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(10) // Replace
+          ),
       //This ensures that the keyboard does not alter the size of the widgets
       resizeToAvoidBottomInset: false,
     );
